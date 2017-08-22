de60e7e493dca92eea581d30aad0a183542251fc
Yuri Pourre <yuripourre@gmail.com>
2017-08-22 12:03:21 -0300

    Fix ApplicationLoader

--- src/main/java/com/harium/etyl/commons/context/load/ApplicationLoader.java
+++ src/main/java/com/harium/etyl/commons/context/load/ApplicationLoader.java
@@ -1,139 +1,127 @@
 package com.harium.etyl.commons.context.load;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import com.harium.etyl.commons.context.Context;
 
-/**
- * 
- * @author yuripourre
- *
- */
+import java.util.concurrent.*;
 
 public class ApplicationLoader {
 
-	private Loader loader = new Loader();
-	private Updater updater = new Updater();
+    private Loader loader = new Loader();
+    private Updater updater = new Updater();
 
-	private Context application;
-	private LoaderListener listener;
-	private DefaultLoadApplication loadApplication;
-	
-	private boolean called = false;
-	private ScheduledExecutorService loadExecutor;
-	private Future<?> future;
-	
-	private static final int UPDATE_INTERVAL = 50;
-	
-	private String lastText = "";
-	private float lastLoading = 0;
-	
-	public ApplicationLoader(int w, int h) {
-		super();
-		
-		loadApplication = new GenericLoadApplication(0, 0, w, h);
-		loadApplication.load();
-	}
+    private Context application;
+    private LoaderListener listener;
+    private DefaultLoadApplication loadApplication;
 
-	public void loadApplication() {
-		lastText = "";
-		lastLoading = 0;
-		
-		loadExecutor = Executors.newScheduledThreadPool(2);
-		
-		future = loadExecutor.submit(loader);
-		loadExecutor.scheduleAtFixedRate(updater, UPDATE_INTERVAL, UPDATE_INTERVAL, TimeUnit.MILLISECONDS);
-	}
+    private boolean called = false;
+    private boolean finished = false;
+    private ScheduledExecutorService loadExecutor;
+    private Future<?> future;
 
-	private class Loader implements Runnable {
+    private static final int UPDATE_INTERVAL = 50;
 
-		public void run() {
-			called = false;
-			application.startLoad();
-			called = true;
-		}
-	}
+    private String lastText = "";
+    private float lastLoading = 0;
 
-	private class Updater implements Runnable {
+    public ApplicationLoader(int w, int h) {
+        super();
 
-		public void run() {
-			if(!called) {
-				
-				if(!application.isLoaded()) {
-					notifyTextChanged();
-					notifyLoadingChanged();
-					
-					getError();
-				}
-				
-			} else {
-				//Notify window
-				finishLoading();
-			}
-		}
-	}
-	
-	private void notifyTextChanged() {
-		String info = application.getLoadingInfo();
-		
-		if(!lastText.equals(info)) {
-			lastText = info;
-			loadApplication.onChangeText(info);
-		}
-	}
-	
-	private void notifyLoadingChanged() {
-		float loading = application.getLoading();
-		
-		if(lastLoading != loading) {
-			lastLoading = loading;
-			loadApplication.onChangeLoad(loading);
-		}
-	}
+        loadApplication = new GenericLoadApplication(0, 0, w, h);
+        loadApplication.load();
+    }
 
-	protected void getError() {
-		try {
-			future.get(UPDATE_INTERVAL/2, TimeUnit.MILLISECONDS);
-		} catch (ExecutionException e) {
-			Throwable cause = e.getCause();
-			cause.printStackTrace();
-			finishLoading();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			//Ignore the Timeout Exception
-			//e.printStackTrace();
-		}
-	}
+    public void loadApplication() {
+        lastText = "";
+        lastLoading = 0;
 
-	private void finishLoading() {
-		listener.onLoad(application);
-	}
-	
-	private void checkForCustomLoadApplication(Context context) {
-		DefaultLoadApplication loadApp = context.getLoadApplication();
-		
-		if(loadApp != null) {
-			this.loadApplication = loadApp;	
-		}
-	}
-	
-	public DefaultLoadApplication reloadApplication(LoaderListener listener, Context context) {		
-		context.setLoaded(false);
-		checkForCustomLoadApplication(context);
-		
-		this.listener = listener;
-		this.application = context;
-		
-		loadApplication.load();
-		loadApplication.setLoaded(true);
-		loadApplication();
-		return loadApplication;
-	}
+        loadExecutor = Executors.newScheduledThreadPool(2);
+
+        future = loadExecutor.submit(loader);
+        loadExecutor.scheduleAtFixedRate(updater, UPDATE_INTERVAL, UPDATE_INTERVAL, TimeUnit.MILLISECONDS);
+    }
+
+    private class Loader implements Runnable {
+        public void run() {
+            finished = false;
+            called = false;
+            application.startLoad();
+            called = true;
+        }
+    }
+
+    private class Updater implements Runnable {
+        public void run() {
+            if (!called) {
+                if (!application.isLoaded()) {
+                    notifyTextChanged();
+                    notifyLoadingChanged();
+
+                    getError();
+                }
+            } else if (!finished) {
+                // Notify Listener
+                finishLoading();
+                finished = true;
+            }
+        }
+    }
+
+    private void notifyTextChanged() {
+        String info = application.getLoadingInfo();
+
+        if (!lastText.equals(info)) {
+            lastText = info;
+            loadApplication.onChangeText(info);
+        }
+    }
+
+    private void notifyLoadingChanged() {
+        float loading = application.getLoading();
+
+        if (lastLoading != loading) {
+            lastLoading = loading;
+            loadApplication.onChangeLoad(loading);
+        }
+    }
+
+    protected void getError() {
+        try {
+            future.get(UPDATE_INTERVAL / 2, TimeUnit.MILLISECONDS);
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            cause.printStackTrace();
+            finishLoading();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            //Ignore the Timeout Exception
+            //e.printStackTrace();
+        }
+    }
+
+    private void finishLoading() {
+        listener.onLoad(application);
+    }
+
+    private void checkForCustomLoadApplication(Context context) {
+        DefaultLoadApplication loadApp = context.getLoadApplication();
+
+        if (loadApp != null) {
+            this.loadApplication = loadApp;
+        }
+    }
+
+    public DefaultLoadApplication reloadApplication(LoaderListener listener, Context context) {
+        context.setLoaded(false);
+        checkForCustomLoadApplication(context);
+
+        this.listener = listener;
+        this.application = context;
+
+        loadApplication.load();
+        loadApplication.setLoaded(true);
+        loadApplication();
+        return loadApplication;
+    }
 
 }
---
+++ src/test/java/com/harium/etyl/commons/context/load/ApplicationLoaderTest.java
@@ -0,0 +1,70 @@
+package com.harium.etyl.commons.context.load;
+
+import com.harium.etyl.commons.context.Application;
+import com.harium.etyl.core.graphics.Graphics;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationLoaderTest {
+
+    private int w = 0, h = 0;
+
+    @Test
+    public void testLoading() {
+        FakeLoaderListener listener = new FakeLoaderListener();
+
+        ApplicationLoader loader = new ApplicationLoader(w, h);
+        loader.reloadApplication(listener, new FakeApplication());
+        loader.loadApplication();
+
+        waitSeconds(3);
+        Assert.assertEquals(1, listener.getCount());
+    }
+
+    private void waitSeconds(int seconds) {
+        int i = 0;
+        while (i < seconds) {
+            i++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class FakeApplication extends Application {
+
+        public FakeApplication() {
+            super(0, 0);
+        }
+
+        @Override
+        public void load() {
+
+        }
+
+        @Override
+        public void draw(Graphics g) {
+
+        }
+    }
+
+    private class FakeLoaderListener implements LoaderListener {
+        int count = 0;
+
+        @Override
+        public void onLoad(Object context) {
+            count++;
+        }
+
+        public void reset() {
+            count = 0;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    };
+
+}
