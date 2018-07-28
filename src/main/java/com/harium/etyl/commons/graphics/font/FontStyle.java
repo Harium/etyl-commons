300ef2065df80e39ed2e4c9e912886e6bf4392cd
Yuri Pourre <yuripourre@gmail.com>
2018-07-28 13:32:21 -0300

    Add Strokes and FontStyle

---
+++ src/main/java/com/harium/etyl/commons/graphics/font/FontStyle.java
@@ -0,0 +1,19 @@
+package com.harium.etyl.commons.graphics.font;
+
+public enum FontStyle {
+    /**
+     * The bold style constant.  This can be combined with the other style
+     * constants (except PLAIN) for mixed styles.
+     */
+    BOLD,
+    /**
+     * The italicized style constant.  This can be combined with the other
+     * style constants (except PLAIN) for mixed styles.
+     */
+    ITALIC,
+
+    /**
+     * The plain style constant.
+     */
+    PLAIN
+}
